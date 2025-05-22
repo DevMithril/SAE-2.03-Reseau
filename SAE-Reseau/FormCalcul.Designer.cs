d36a8b1fe819a829f940306c89d320569e0eaf15
DevMithril <tbruyelles@gmail.com>
2025-05-22 18:34:36 +0200

    Ajoutez des fichiers projet.

---
+++ SAE-Reseau/FormCalcul.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SAE_Reseau
+{
+    partial class FormCalcul
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCompute = new Button();
+            txtInput = new TextBox();
+            txtOutput = new TextBox();
+            lblNomsParticipants = new Label();
+            lblMetadata = new Label();
+            lblError = new Label();
+            SuspendLayout();
+            // 
+            // btnCompute
+            // 
+            btnCompute.Location = new Point(428, 278);
+            btnCompute.Name = "btnCompute";
+            btnCompute.Size = new Size(188, 23);
+            btnCompute.TabIndex = 0;
+            btnCompute.Text = "Calculer le Header Checksum";
+            btnCompute.UseVisualStyleBackColor = true;
+            btnCompute.Click += btnCompute_Click;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(298, 26);
+            txtInput.Multiline = true;
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(469, 218);
+            txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(440, 354);
+            txtOutput.Name = "txtOutput";
+            txtOutput.ReadOnly = true;
+            txtOutput.Size = new Size(159, 23);
+            txtOutput.TabIndex = 2;
+            // 
+            // lblNomsParticipants
+            // 
+            lblNomsParticipants.AutoSize = true;
+            lblNomsParticipants.Location = new Point(45, 52);
+            lblNomsParticipants.Name = "lblNomsParticipants";
+            lblNomsParticipants.Size = new Size(178, 30);
+            lblNomsParticipants.TabIndex = 3;
+            lblNomsParticipants.Text = "Vermeulen - Vaucher - Levasseur\r\nGianessi - Bruyelles";
+            lblNomsParticipants.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblMetadata
+            // 
+            lblMetadata.AutoSize = true;
+            lblMetadata.Location = new Point(27, 123);
+            lblMetadata.Name = "lblMetadata";
+            lblMetadata.Size = new Size(224, 45);
+            lblMetadata.TabIndex = 4;
+            lblMetadata.Text = "Année Scolaire 2024-2025\r\nIUT d'Amiens, Département Informatique\r\nSAE Réseau Semestre 2";
+            lblMetadata.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblError
+            // 
+            lblError.AutoSize = true;
+            lblError.Location = new Point(86, 354);
+            lblError.Name = "lblError";
+            lblError.Size = new Size(48, 15);
+            lblError.TabIndex = 5;
+            lblError.Text = "Error lol";
+            // 
+            // FormCalcul
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblError);
+            Controls.Add(lblMetadata);
+            Controls.Add(lblNomsParticipants);
+            Controls.Add(txtOutput);
+            Controls.Add(txtInput);
+            Controls.Add(btnCompute);
+            Name = "FormCalcul";
+            Text = "Calculateur de Header Checksum";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCompute;
+        private TextBox txtInput;
+        private TextBox txtOutput;
+        private Label lblNomsParticipants;
+        private Label lblMetadata;
+        private Label lblError;
+    }
+}
---
+++ SAE-Reseau/FormCalcul.cs
@@ -0,0 +1,125 @@
+using System.Diagnostics;
+
+namespace SAE_Reseau
+{
+    public partial class FormCalcul : Form
+    {
+        public FormCalcul()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Convertit un code hexadécimal en entier
+        /// </summary>
+        /// <param name="str_hex">Chaîne de caractères contenant un code hexadécimal</param>
+        /// <returns>L'entier correspondant à la valeur du code hexadécimal</returns>
+        private static int ConvertToInt(string str_hex)
+        {
+            int int_hex = 0;
+
+            // pour chaque caractère de la chaîne
+            foreach (char car in str_hex)
+            {
+                // on multiplie par 16 la valeur précédente
+                int_hex *= 16;
+
+                // on ajoute la valeur du caractère actuel
+                if (car <= '9' && car >= '0')
+                {
+                    int_hex += car - '0';
+                }
+                else
+                {
+                    int_hex += (car - 'A') + 10;
+                }
+            }
+
+            return int_hex;
+        }
+
+        /// <summary>
+        /// Convertit un entier en code hexadécimal
+        /// </summary>
+        /// <param name="int_hex">Entier à convertir</param>
+        /// <returns>La chaîne de caractères contenant le code hexadécimal de l'entier</returns>
+        private static string ConvertToString(int int_hex)
+        {
+            string str_hex = string.Empty;
+            int dec;
+            char hex;
+
+            // tant qu'il reste une valeur à convertir
+            while (int_hex != 0)
+            {
+                // on calcul la valeur décimale du prochain caractère hexadécimal
+                dec = int_hex % 16;
+
+                // on convertit en hexadécimal la valeur décimale
+                hex = (char) (dec +
+                        ((dec >= 10) ? 'A' - 10 : '0'));
+
+                // on ajoute le nouveau caractère au début de la chaîne
+                str_hex = hex + str_hex;
+                
+                // on divise par 16 pour passer au caractère suivant
+                int_hex /= 16;
+            }
+
+            return str_hex;
+        }
+
+        /// <summary>
+        /// Calcule le Header Checksum d'une entête IP
+        /// </summary>
+        /// <param name="input">Chaîne de caractères contenant l'entête IP sous forme hexadécimale</param>
+        /// <returns>Chaîne de caractères contenant le Header Checksum sous forme hexadécimale</returns>
+        private static string ComputeHeaderChecksum(string input)
+        {
+            // on standardise la chaîne de caractères
+            input = input.Replace(" ", "");
+            input = input.Replace("\r\n", "");
+            input = input.Replace(";", "");
+            input = input.Replace(",", "");
+            input = input.Replace(".", "");
+            input = input.ToUpper();
+
+            // on vérifie que la chaîne est valide
+            if (input.Length % 4 != 0)
+            {
+                throw new ArgumentException("L'entête IP doit contenir un nombre pair d'octets hexadécimaux.");
+            }
+
+            // initialise la somme à 0
+            int output = 0;
+
+            // on additionne les valeurs hexadécimales en ignorant la valeur du checksum
+            for (int i = 0; i < input.Length; i+=4)
+            {
+                if (i == 20)
+                    continue;
+                output += ConvertToInt(input.Substring(i, 4));
+            }
+
+            // ajout des bits de l'overflow
+            output = output % 65536 + output / 65536;
+            
+            // inversion des bits
+            output = 65535 - output;
+
+            return ConvertToString(output);
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                txtOutput.Text = ComputeHeaderChecksum(txtInput.Text);
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+        }
+    }
+}
